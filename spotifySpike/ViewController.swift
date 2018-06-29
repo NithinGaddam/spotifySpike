92d4c4e3e627f7c0908939bc518aa712b7d0db85
Nithin Gaddam <ngaddam@logitech.com>
2018-06-29 10:37:22 -0700

    Initial Commit

---
+++ spotifySpike/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  spotifySpike
+//
+//  Created by Nithin Gaddam on 6/29/18.
+//  Copyright © 2018 Nithin Gaddam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
